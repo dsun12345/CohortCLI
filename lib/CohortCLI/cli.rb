e89abae4c1d211660aad7e07aafb5b9bf97d9457
Daniel Sun <danielsun12345@gmail.com>
2020-02-26 22:32:22 +0000

    add notes for Cli class

--- lib/CohortCLI/cli.rb
+++ lib/CohortCLI/cli.rb
@@ -1,8 +1,32 @@
 class CohortCLI::CLI
   
-  def start 
+  #start should great user, maybe give description
+  # get data from scraper/api file
+  #create new custom objects
+  # all inside start method 
+  
+   def start 
     puts "Hello there!"
+    puts "Getting Data From API ... Please wait"
+    puts "Creating new OBJS"
+    
     binding.pry
   end 
+  
+  
+  
+  # deal with inputs (loop to keep asking to get new info)
+  # Ex. while input != "exit" do 
+  # display a list of something, or give examples of what we expect as input 
+  # get user input 
+  # depending on what we get, do something 
+  # condition to check input for good value 
+  # else tell them try again 
+  
+  # exit command 
+  # if input == "EXIT"
+  # kill program, say good bye 
+  
+ 
 
 end 
