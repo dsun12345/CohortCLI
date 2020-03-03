41db02766b680e46721822249b003c8b4af00908
Daniel Sun <danielsun12345@gmail.com>
2020-03-03 02:58:08 +0000

    changes after crash

--- lib/CohortCLI/api.rb
+++ lib/CohortCLI/api.rb
@@ -4,17 +4,15 @@ class CohortCLI::API
 
   def self.get_character
     @character_hash = HTTParty.get("https://anapioficeandfire.com/api/characters/583")
-    binding.pry 
-    
     character_obj = {
       name: @character_hash["name"],
       gender: @character_hash["gender"],
       culture: @character_hash["culture"],
       titles: @character_hash["titles"],
       playedBy: @character_hash["playedBy"]
-      
     }
     
+    binding.pry 
   end 
 
 
